073b8972316f87612448c9775bc0e232ddd28df2
navee <navee@LAPTOP-OHQ3ME1T>
2022-06-10 11:42:44 +0530

    control statements

--- chaihanyaassignment/src/chaihanyaassignment/assignment.java
+++ chaihanyaassignment/src/chaihanyaassignment/assignment.java
@@ -3,7 +3,18 @@ package chaihanyaassignment;
 public class assignment {
 
 	public static void main(String[] args) {
+		System.out.println("HELLO PRINT");
+		
+		
 		// TODO Auto-generated method stub
+		int time = 22;
+		if (time < 10) {
+		  System.out.println("Good morning.");
+		} else if (time < 20) {
+		  System.out.println("Good day.");
+		} else {
+		  System.out.println("Good evening.");
+		}
 
 	}
 
